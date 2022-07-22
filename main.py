dad0c6b178d57fb2c29207fd8b7472a5ccdb5222
Marcel Koopman <marcel.koopman@gmail.com>
2022-07-22 15:42:00 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,42 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import locale
+import logging
+
 locale.setlocale(locale.LC_ALL, "nl_NL")
 
-df = pd.read_csv("data/mijn-meterstanden.csv", encoding = "ISO-8859-1").head(30)
+def create_dataframe_from_csv():
+    df = pd.read_csv("data/mijn-meterstanden.csv", encoding = "ISO-8859-1").head(30)
+    df['Stroom'] = df['Stroom 1 (kWh)'] + df['Stroom 2 (kWh)']
+    df['Teruglevering'] = df['Teruglevering 1 (kWh)'] + df['Teruglevering 2 (kWh)']
+    df['change'] = df['Teruglevering'] - df['Stroom']
+    df['Meetdatum'] = df['Meetdatum'].str.replace(".", "")
+    df['Datum'] = pd.to_datetime(df['Meetdatum'], format='%d %b %Y')
+    df = df.sort_values(by=['Datum'], ascending = True)
+    df = df.query('Stroom>0')
+    df['pct_stroom'] = df['Stroom'].pct_change()
+    df['pct_teruglevering'] = df['Teruglevering'].pct_change()
+    df['diff_stroom'] = df['Stroom'].diff().abs()
+    df['diff_teruglevering'] = df['Teruglevering'].diff().abs()
+    df['median_teruglevering'] = df['diff_teruglevering'].median()
+    df['mean_teruglevering'] = df['diff_teruglevering'].mean()
+    df['mean_stroom'] = df['diff_stroom'].mean()
+    return df
 
-df['Stroom'] = df['Stroom 1 (kWh)'] + df['Stroom 2 (kWh)']
-df['Teruglevering'] = df['Teruglevering 1 (kWh)'] + df['Teruglevering 2 (kWh)']
-df['change'] = df['Teruglevering'] - df['Stroom']
-df['Meetdatum'] = df['Meetdatum'].str.replace(".", "")
-df['Datum'] = pd.to_datetime(df['Meetdatum'], format='%d %b %Y')
-df = df.sort_values(by=['Datum'], ascending = True)
-print(df)
-# df.info()
-df = df.query('Stroom>0')
-df['pct_stroom'] = df['Stroom'].pct_change()
-df['pct_teruglevering'] = df['Teruglevering'].pct_change()
-df['diff_stroom'] = df['Stroom'].diff().abs()
-df['diff_teruglevering'] = df['Teruglevering'].diff().abs()
-df['median_teruglevering'] = df['diff_teruglevering'].median()
-df['mean_teruglevering'] = df['diff_teruglevering'].mean()
-df['mean_stroom'] = df['diff_stroom'].mean()
+def select_maximum(df):
+    maxidx = df['diff_stroom'].idxmax()
+    maximmum = df.loc[maxidx]
+    print(f'max stroom {maximmum}')
 
-maxidx = df['diff_stroom'].idxmax()
-maximmum = df.loc[maxidx]
-# print(f'max stroom {maximmum}')
-#  print(df[['pct_teruglevering', 'mean_teruglevering', 'Teruglevering']])
-# print(df[['mean_teruglevering', 'mean_stroom']])
-# df.info()
+df = create_dataframe_from_csv().head(10)
+df.info()
 x = df['Meetdatum']
-# print(df)
-# plt.plot(x, df['Stroom'], label="Stroomverbruik")
-# plt.plot(x, df['diff_teruglevering'], label="Teruglevering")
 plt.plot(x, df['change'], label='Efficiency')
-# plt.plot(x, df['Teruglevering'], label="Teruglevering")
-# plt.plot(x, df['Gas 1 (m³)'], label="Gasverbruik")
-# plt.plot_date(x, df['Gas 1 (m³)'])
-# plt.plot_date(x, df['Stroom'])
 
 plt.legend()
 plt.show()
--- main.sh
+++ main.sh
@@ -2,4 +2,5 @@
 python3 -m venv ./venv
 source ./venv/bin/activate
 pip3 install pandas matplotlib
+venv/bin/python3 -m pip install --upgrade pip
 venv/bin/python3 main.py
