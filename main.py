e7fbaf08bff29b4ba5ffa14633325a0bc03cd336
Marcel Koopman <marcel.koopman@gmail.com>
2022-07-22 15:49:59 +0200

    refactoring

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ import locale
 import logging
 
 locale.setlocale(locale.LC_ALL, "nl_NL")
+logger = logging.getLogger('log')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 def create_dataframe_from_csv():
     df = pd.read_csv("data/mijn-meterstanden.csv", encoding = "ISO-8859-1").head(30)
     df['Stroom'] = df['Stroom 1 (kWh)'] + df['Stroom 2 (kWh)']
     df['Teruglevering'] = df['Teruglevering 1 (kWh)'] + df['Teruglevering 2 (kWh)']
     df['change'] = df['Teruglevering'] - df['Stroom']
-    df['Meetdatum'] = df['Meetdatum'].str.replace(".", "")
+    df['Meetdatum'] = df['Meetdatum'].str.replace(".", "", 1, regex=False)
     df['Datum'] = pd.to_datetime(df['Meetdatum'], format='%d %b %Y')
+    df = df.drop("Meetdatum", axis='columns')
     df = df.sort_values(by=['Datum'], ascending = True)
     df = df.query('Stroom>0')
     df['pct_stroom'] = df['Stroom'].pct_change()
@@ -26,11 +34,14 @@ def create_dataframe_from_csv():
 def select_maximum(df):
     maxidx = df['diff_stroom'].idxmax()
     maximmum = df.loc[maxidx]
-    print(f'max stroom {maximmum}')
+    logger.info(f'max stroom {maximmum}')
 
+logger.info("Parsing CSV")
 df = create_dataframe_from_csv().head(10)
-df.info()
-x = df['Meetdatum']
+logger.info("Dataframe created")
+select_maximum(df)
+# df.info()
+x = df['Datum']
 plt.plot(x, df['change'], label='Efficiency')
 
 plt.legend()
